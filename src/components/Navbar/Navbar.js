97bff423de3e1008636ff3307be70d789d46ba96
inknsharps <nchen25@gmail.com>
2021-06-06 21:14:27 -0400

    add bootstrap, add basic navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = ({navbarTheme, backgroundColor}) => {
+    return (
+        <nav className={`navbar navbar-${navbarTheme} bg-${backgroundColor}`}>
+            <div className="container-fluid">
+                <span className="navbar-brand mb-0 h1">Navbar</span>
+            </div>
+        </nav>
+    )
+}
+
+Navbar.defaultProps = {
+    navbarTheme: "light",
+    backgroundColor: "light"
+}
+
+export default Navbar;
