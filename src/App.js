8d7b3435590970a42a1701bfc78a1fd8a7f382f5
inknsharps <nchen25@gmail.com>
2021-06-06 21:14:51 -0400

    add bootstrap, add basic navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar
+        navbarTheme="dark"
+        backgroundColor="primary"
+      />
       <h1>Hello World!</h1>
     </div>
   );
