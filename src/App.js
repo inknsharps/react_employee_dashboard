cf887c3daf98f8c9d2f66762c3c3c282a274d44b
inknsharps <nchen25@gmail.com>
2021-06-06 21:33:03 -0400

    format file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import './App.css';
+import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar
-        navbarTheme="dark"
-        backgroundColor="primary"
-      />
-      <h1>Hello World!</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar navbarTheme="dark" backgroundColor="primary" />
+            <h1>Hello World!</h1>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./Navbar.css";
 
-const Navbar = ({navbarTheme, backgroundColor}) => {
-    return (
-        <nav className={`navbar navbar-${navbarTheme} bg-${backgroundColor}`}>
-            <div className="container-fluid">
-                <span className="navbar-brand mb-0 h1">Navbar</span>
-            </div>
-        </nav>
-    )
-}
+const Navbar = ({ navbarTheme, backgroundColor }) => {
+	return (
+		<nav className={`navbar navbar-${navbarTheme} bg-${backgroundColor}`}>
+			<div className="container-fluid">
+				<span className="navbar-brand mb-0 h1">Navbar</span>
+			</div>
+		</nav>
+	);
+};
 
 Navbar.defaultProps = {
-    navbarTheme: "light",
-    backgroundColor: "light"
-}
+	navbarTheme: "light",
+	backgroundColor: "light"
+};
 
 export default Navbar;
