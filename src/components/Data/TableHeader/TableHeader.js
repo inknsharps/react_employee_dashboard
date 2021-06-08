227d01d105f429e4b9b23def5cab9b3f673505f1
inknsharps <nchen25@gmail.com>
2021-06-08 10:47:06 -0400

    remove testing click handler

--- src/components/Data/TableHeader/TableHeader.js
+++ src/components/Data/TableHeader/TableHeader.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./TableHeader.css";
-import handleClick from "../../../utils/handleClick";
 
 const TableHeader = () => {
     return (
         <thead className="TableHeader">
-            <tr onClick={handleClick}>
+            <tr>
                 <th scope="col">#</th>
                 <th scope="col">Image</th>
                 <th scope="col">Name</th>
